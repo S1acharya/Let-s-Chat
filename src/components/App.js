8dfdfbd327a7ef8377d8eba33d533564581f3166
S1Acharya123 <saketacharya.08@gmail.com>
2021-07-09 17:40:52 +0530

    login page created

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 // import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// react-router-dom is a library which allows to make multi page application using react
 
 // import { AuthProvider } from "../contexts/AuthContext"
 
 // import Chats from "./Chats"
-// import Login from "./Login"
+import Login from "./Login"
 
 function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
     <h1>hello</h1>
       <Router>
-        {/* <AuthProvider> */}
+        {/* <AuthProvider> */}  \\it will wrap everything coming react context API
           <Switch>
-            {/* <Route path="/chats" component={Chats} />
-            <Route path="/" component={Login} /> */}
+            {/* <Route path="/chats" component={Chats} /> */}
+            <Route path="/" component={Login} /> 
           </Switch>
         {/* </AuthProvider> */} 
       </Router>
---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {GoogleOutlined , FacebookOutlined} from '@ant-design/icons';
+
+// functional component
+const Login = () => {
+    return (
+        <div id = "login-page">
+            <div id = "login-card">
+                <h2>Welcome to Let's Chat!</h2>
+
+                <div className = "login-button google">
+                    <GoogleOutlined /> Sign In with Google
+                </div>
+
+                <br/> <br/>
+
+                <div className = "login-button facebook">
+                    <FacebookOutlined /> Sign In with facebook
+                </div>
+            </div>
+        </div>
+    );
+// here we will have JSX for our form with our 2 buttons(facebook and google login)
+}
+
+export default Login;
