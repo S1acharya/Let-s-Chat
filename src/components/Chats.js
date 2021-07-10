dedc77ffe7ac97fe86299b142164c36221d79ca1
S1Acharya123 <saketacharya.08@gmail.com>
2021-07-10 11:58:21 +0530

    chat engine initialised

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine} from 'react-chat-engine';
+import { auth } from '../firebase';
 
 const Chats = () => {
+    const history = useHistory();
+
+    const handleLogout = async() => {
+        await auth.signOut();
+        history.push('/');  //this takes the user back to login page
+    }
+
     return (
-        <h1>Chats</h1>
+        <div className = "chats-page">
+            <div className = "nav-bar">
+                <div className = "logo-tab">
+                    Let's Chat
+                </div>
+
+                <div className = "center-tab">
+                    🔥🔥Securly Connecting!🔥🔥
+                </div>
+
+                <div onClick = { handleLogout } className = "logout-tab">
+                    logout
+                </div>
+            </div>
+
+            <ChatEngine
+                height = "calc(100vh - 100px)"
+                projectId ="576d8e92-047c-4f16-9488-f1e2a0db6e8a"
+                userName = "."
+                userSecret = "."
+            />
+        </div>
     );
 }
 
